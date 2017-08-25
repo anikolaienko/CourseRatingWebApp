40b8edd454a990992e69267ab6263520a058a01e
Andrii <nikolayenko.andrew@gmail.com>
2017-08-25 15:55:37 +0300

    added bootstrap template

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('Hello world');
+});
+
+app.get('/courses', function(req, res){
+    res.send('Hello courses');
+});
+
+app.listen(5000, function(err){
+    console.log('running server on port ' + port);
+});
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
+
+  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest('vendor/popper'))
+})
+
+// Default task
+gulp.task('default', ['copy']);
+
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+  // Reloads the browser whenever HTML or CSS files change
+  gulp.watch('css/*.css', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+});
